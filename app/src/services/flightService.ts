0c8333d29120c17c513c3ebff3e1ca4fd15804b9
Bar Almog <baralmog@protonmail.com>
2024-08-27 21:37:44 +0300

    seperated query limit

--- app/src/services/flightService.ts
+++ app/src/services/flightService.ts
@@ -3,13 +3,14 @@ import { Flight } from "../types/Flight";
 
 const FLIGHTS_API_URL = "https://data.gov.il/api/3/action/datastore_search";
 const RESOURCE_ID = "e83f763b-b7d7-479e-b172-ae981ddc6de5";
+const QUERY_LIMIT = 300;
 
 export async function fetchFlightData(q?: string): Promise<Flight[]> {
   try {
     const response = await axios.get(FLIGHTS_API_URL, {
       params: {
         resource_id: RESOURCE_ID,
-        limit: 300,
+        limit: QUERY_LIMIT,
         q: q,
       },
     });
